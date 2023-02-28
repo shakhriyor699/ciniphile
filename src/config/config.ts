b7318a92db84fc12c52de1c1013ba9c3c9f4485e
sh <sh@gmail.com>
2023-02-28 13:04:21 +0500

    add banner components & config for img

---
+++ src/config/config.ts
@@ -0,0 +1,2 @@
+export const img_original: string = 'https://image.tmdb.org/t/p/original/'
+export const img_500: string = "https://image.tmdb.org/t/p/w500/";
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit"
 
 
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    
+
   },
   devTools: true
 })
 
 
+export default store
