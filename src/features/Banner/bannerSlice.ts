f175df9bd29e4a4aa1f102a41e3ec806bcfb4297
sh <sh@gmail.com>
2023-03-01 13:28:52 +0500

    created async request

--- src/features/Banner/bannerSlice.ts
+++ src/features/Banner/bannerSlice.ts
@@ -1,6 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+
+interface IUpComingState {
+  loading: boolean,
+  list: unknown[],
+  error: unknown
+}
+
+type Banner = {
+
+}
+
+export const loadUpComingBanner = createAsyncThunk(
+  '@@upcoming/loadBanner',
+  async (_, { dispatch }) => {
+
+  }
+)
+
+const initialState: IUpComingState = {
+  loading: true,
+  list: [],
+  error: null
+}
 
 const bannerSlice = createSlice({
-  
+  name: '@@upcoming',
+  initialState,
+  reducers: {
+
+  }
 })
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,10 +4,13 @@ import { configureStore } from "@reduxjs/toolkit"
 
 const store = configureStore({
   reducer: {
-
+    
   },
   devTools: true
 })
 
 
 export default store
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
