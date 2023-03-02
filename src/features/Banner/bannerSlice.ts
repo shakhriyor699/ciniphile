9f542487fb45410be798b97783f68a5c228416d4
sh <sh@gmail.com>
2023-03-02 13:46:49 +0500

    created async request

--- src/features/Banner/bannerSlice.ts
+++ src/features/Banner/bannerSlice.ts
@@ -1,26 +1,24 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import { IUpComingState, UpComingTypes } from "../../types/types";
 
-
-interface IUpComingState {
-  loading: boolean,
-  list: unknown[],
-  error: unknown
-}
-
-type Banner = {
-
-}
 
 export const loadUpComingBanner = createAsyncThunk(
   '@@upcoming/loadBanner',
-  async (_, { dispatch }) => {
+  async (_, {  dispatch, rejectValue }) => {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=9976e0dfa6f1817cd89602c8ff3fd6d7&language=ru-RU&page=1`)
+    let { data } = res
+    dispatch(addUpcomingMovie(data))
 
+    if (!res) {
+      rejectValue('error')
+    }
   }
 )
 
 const initialState: IUpComingState = {
   loading: true,
-  list: [],
+  list: null,
   error: null
 }
 
@@ -28,6 +26,11 @@ const bannerSlice = createSlice({
   name: '@@upcoming',
   initialState,
   reducers: {
-
+    addUpcomingMovie: (state, action: PayloadAction<UpComingTypes>) => {
+      state.list = action.payload
+    }
   }
 })
+
+
+const { addUpcomingMovie } = bannerSlice.actions
--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,1 +1,18 @@
-export {}
+
+export type UpComingTypes = {
+  dates: {
+    maximum: string
+    minimum: string
+  },
+  page: number,
+  results: [],
+  total_pages: number,
+  total_results: number
+}
+
+
+export interface IUpComingState {
+  loading: boolean,
+  list: UpComingTypes | null,
+  error: string | null
+}
