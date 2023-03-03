3b795f7e5a4e90dec272cbc4e9ee12f6e41aa937
sh <sh@gmail.com>
2023-03-03 10:09:43 +0500

    add types axios

--- src/features/Banner/bannerSlice.ts
+++ src/features/Banner/bannerSlice.ts
@@ -5,13 +5,13 @@ import { IUpComingState, UpComingTypes } from "../../types/types";
 
 export const loadUpComingBanner = createAsyncThunk(
   '@@upcoming/loadBanner',
-  async (_, {  dispatch, rejectValue }) => {
-    const res = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=9976e0dfa6f1817cd89602c8ff3fd6d7&language=ru-RU&page=1`)
+  async (_, { dispatch, rejectWithValue }) => {
+    const res = await axios.get<UpComingTypes>(`https://api.themoviedb.org/3/movie/upcoming?api_key=9976e0dfa6f1817cd89602c8ff3fd6d7&language=ru-RU&page=1`)
     let { data } = res
     dispatch(addUpcomingMovie(data))
 
     if (!res) {
-      rejectValue('error')
+      rejectWithValue('error')
     }
   }
 )
